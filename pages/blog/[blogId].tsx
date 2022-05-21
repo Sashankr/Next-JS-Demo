e31b26767d3819cf3bcab634afcf0865ac7212b2
Sashankr <sashankr97@gmail.com>
2022-05-21 23:37:31 +0530

    get static paths

--- pages/blog/[blogId].tsx
+++ pages/blog/[blogId].tsx
@@ -1,13 +1,35 @@
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
+import { GetStaticPaths, GetStaticProps } from "next";
 
+export const getStaticProps: GetStaticProps = async (context) => {
+  return {
+    props: {
+      myNumber: Math.random(),
+    },
+  };
+};
 
-export default function IndividualBlog(){
-    
-    const router = useRouter();
-    console.log(router);
-    
-    
-    return (
-        <h1>These are individual blogs</h1>
-    )
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      {
+        params: { blogId: "1" },
+      },
+      {
+        params: { blogId: "2" },
+      },
+    ],
+    fallback: true,
+  };
+};
+
+export default function IndividualBlog(props) {
+  const router = useRouter();
+  console.log(router);
+
+  if(router.isFallback){
+      return <h1>Loading .....</h1>
+  }
+
+  return <h1>These are individual blogs {props.myNumber}</h1>;
 }
